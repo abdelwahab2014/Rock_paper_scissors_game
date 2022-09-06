94e780199b024dc4dd69f5154c9db6f4d487f09f
me <data28448802@gmail.com>
2022-09-06 13:51:50 +0200

    UI DESIGN FOR GAME

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container bg">
-game
+<div className="container text-center">
+  <div className=" ">
+    <div className="col-lg-12 col-md-12 col-sm-12 bg1 div_height "><img  alt="" src="https://img.icons8.com/external-tulpahn-detailed-outline-tulpahn/64/000000/external-paper-stationery-tulpahn-detailed-outline-tulpahn.png"/></div>
+    <div className = "row col-lg-12 col-md-12 col-sm-12">
+    <div className="bg2 div_width div_height"><img src="https://img.icons8.com/emoji/96/000000/rock-emoji.png" alt=""/></div>
+    <div className="bg div_width div_height"><img src="https://img.icons8.com/carbon-copy/100/000000/barber-scissors.png" alt=""/></div>
+    
+
     </div>
+    
+   
+  </div>
+</div>
   );
 }
 
