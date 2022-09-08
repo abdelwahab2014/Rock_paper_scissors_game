82c4380710f317468d61519aefc4739675f3eb8e
me <data28448802@gmail.com>
2022-09-08 17:32:06 +0200

    win lose icon

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React, { useState } from 'react';
 const list  = ["Paper", "Rock", "Scissors"]
 
 function App (){
-  var [selection, selectionState] = useState(0);
+ var [selection, selectionState] = useState(0);
  var r = Math.floor(Math.random()*3) + 1;
+ 
     return (
       <div className="container text-center">
        <div className='score'>
@@ -15,37 +16,41 @@ function App (){
             case 1:
                 if(selection === 1){
                 return (
-                    <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/> Draw !</div>
+                    <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/> Draw !
+                    <br/><img className='image_size' src="https://media.giphy.com/media/LXHJRRjnviw7e/giphy.gif" />
+                    </div>
                 )}
                 if(selection === 2){
                 return (
                     <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>
-                    You Lose</div>
+                    You Lose<br/><img className='image_size' src="https://media.giphy.com/media/BEob5qwFkSJ7G/giphy.gif" /></div>
                 )}
 
                 if(selection === 3){
                 return (
                     <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>
-                    You Win !</div>
+                    You Win !<br/><img className='image_size' src="https://media.giphy.com/media/TdfyKrN7HGTIY/giphy.gif" /></div>
                 )}
-                
+            
                break;
            case 2: /*Case 2 */
            if(selection === 2){
                 return (
-                    <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>Draw !</div>
+                    <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>Draw !
+                    <br/><img className='image_size' src="https://media.giphy.com/media/LXHJRRjnviw7e/giphy.gif" />
+                    </div>
                 )}
             
            if(selection === 1){
                 return (
                     <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>
-                    You Win !</div>
+                    You Win !<br/><img className='image_size' src="https://media.giphy.com/media/TdfyKrN7HGTIY/giphy.gif" /></div>
                 )}
             
           if(selection === 3){
                 return (
                     <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>
-                    You Lose</div>
+                    You Lose <br/><img className='image_size' src="https://media.giphy.com/media/BEob5qwFkSJ7G/giphy.gif" /></div>
                 )}
             
 
@@ -54,19 +59,21 @@ function App (){
            case 3:/*Case 2 */
            if(selection === 3){
                 return (
-                    <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>Draw !</div>
+                    <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>Draw !
+                    <br/><img className='image_size' src="https://media.giphy.com/media/LXHJRRjnviw7e/giphy.gif" />
+                    </div>
                 )}
 
           if(selection === 1){
                 return (
                     <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>
-                    You Lose</div>
+                    You Lose <br/><img className='image_size' src="https://media.giphy.com/media/BEob5qwFkSJ7G/giphy.gif" /></div>
                 )}
 
           if(selection === 2){
                 return (
                     <div>Your Selection is {list[selection-1]} <br/> Com Selection is {list[r-1]} <br/>
-                   You Win !</div>
+                   You Win !<br/><img className='image_size' src="https://media.giphy.com/media/TdfyKrN7HGTIY/giphy.gif" /></div>
                 )}
             break;
             default:selection=0;
@@ -78,20 +85,20 @@ function App (){
 
 
 
-
-
-
-
         <div>
           <div className="col-lg-12 col-md-12 col-sm-12 div_height ">
           <img  alt="" src="https://img.icons8.com/external-tulpahn-detailed-outline-tulpahn/64/000000/external-paper-stationery-tulpahn-detailed-outline-tulpahn.png"
-            onClick = {()=> selectionState(selection = 1)}
+            onClick = {() => selectionState(selection = 1)}
           /></div>
 
           <div className = "row col-lg-12 col-md-12 col-sm-12">
-          <div className="div_width div_height"><img src="https://img.icons8.com/emoji/96/000000/rock-emoji.png" alt=""
+          
+          <div className="div_width div_height">
+          <img src="https://img.icons8.com/emoji/96/000000/rock-emoji.png" alt=""
             onClick = {()=> selectionState(selection = 2)}
-          /></div>
+          />
+          </div>
+
           <div className=" div_width div_height"><img src="https://img.icons8.com/carbon-copy/100/000000/barber-scissors.png" 
           alt="" onClick = {()=> selectionState(selection = 3)}/></div>
           
